deebcafedf12caead8bcd47ffba7f5d45b1793e7
Your Name <you@example.com>
2019-05-14 14:17:28 +0300

    first sockets

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {serverClient: true});
+
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: __dirname });
 });
 
-app.use(express.static('chat'));
-const server = app.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
+io.on('connection', function (socket) {
+    socket.emit('connected', 'Oksana,you are connected');
 });
+
+app.use(express.static('chat'));
+app.listen(7777, () => {
+    console.log('server is running on port 7777');
+});
+
+
