16f8903101b8acc364b387da106ad44e176157dd
Your Name <you@example.com>
2019-05-14 10:45:06 +0300

    sendFile works good

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname });
+});
+
 app.use(express.static('chat'));
 const server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
